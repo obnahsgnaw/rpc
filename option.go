eb05d7910683ac9d3d1d3957363896645544695a
obnahsgnaw <obnahsgnaw@qq.com>
2023-12-22 19:54:03 +0800

    fix(rpc): fix parent

--- option.go
+++ option.go
@@ -1,21 +1,9 @@
 package rpc
 
-import (
-	"github.com/obnahsgnaw/application"
-)
-
 type Option func(s *Server)
 
 func RegEnable() Option {
 	return func(s *Server) {
 		s.regEnable = true
-	}
-}
-func Parent(p application.Server) Option {
-	return func(s *Server) {
-		s.pServer = p
-		if _, ok := s.regInfos[s.id]; ok {
-			s.regInfos[s.id].ServerInfo.Type = p.Type().String()
-		}
 	}
 }
